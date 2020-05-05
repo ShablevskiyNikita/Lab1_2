256b085dceb1d8eb15570ce2d7e49434727a5ea6
Nikita Shablevskyi <chitbomb@gmail.com>
2020-05-05 14:20:55 +0300

    Finished first task

---
+++ Lab1_2(1)/Lab1_2/Form3.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab1_2
+{
+    public partial class Form3 : Form
+    {
+        Form2 form2Ref;
+        public Form3(Form2 form2Ref)
+        {
+            this.form2Ref = form2Ref;
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            form2Ref.Close();
+            Close();
+        }
+    }
+}
