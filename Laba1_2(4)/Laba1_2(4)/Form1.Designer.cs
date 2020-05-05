ed6e328538300c13dfe6bddc884ce83e4232d7c9
Nikita Shablevskyi <chitbomb@gmail.com>
2020-05-05 15:42:09 +0300

    Fourth task finished

---
+++ Laba1_2(4)/Laba1_2(4)/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace Laba1_2_4_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lastClicked_label = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(249, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(108, 40);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Add label";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(192, 77);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(117, 24);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Last clicked :";
+            // 
+            // lastClicked_label
+            // 
+            this.lastClicked_label.AutoSize = true;
+            this.lastClicked_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lastClicked_label.Location = new System.Drawing.Point(315, 77);
+            this.lastClicked_label.Name = "lastClicked_label";
+            this.lastClicked_label.Size = new System.Drawing.Size(0, 24);
+            this.lastClicked_label.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(382, 450);
+            this.Controls.Add(this.lastClicked_label);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lastClicked_label;
+    }
+}
+
---
+++ Laba1_2(4)/Laba1_2(4)/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Laba1_2_4_
+{
+    public partial class Form1 : Form
+    {
+        int labelsNumber = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Label newLabel = new Label();
+            labelsNumber++;
+            newLabel.Name = $"{labelsNumber}";
+            newLabel.Text = "Label" + labelsNumber;
+            newLabel.Font = new Font("Arial", 15, FontStyle.Regular);
+            newLabel.Location = new Point(40, 20 + labelsNumber * 30);
+            newLabel.MouseClick += new MouseEventHandler(handleClick);
+
+            Controls.Add(newLabel);
+        }
+
+        public void handleClick(object sender,EventArgs e)
+        {
+            Label clicked = (Label)sender;
+            lastClicked_label.Text = clicked.Name;
+        }
+    }
+}
